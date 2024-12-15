eed9ac34eecdefa1532c209ca56711cb1eddc5c5
Zehra Demir <zehrademir.gm@gmail.com>
2024-12-15 18:07:39 +0300

    Create Product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,11 @@
+// backend/models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Product', productSchema);
